0a28b789ac837a17a8c2bbf33e5e7a5bf1637f84
Bill Hickey <bhickey77@gmail.com>
2018-06-26 11:42:36 -0500

    added redux logger

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <button onClick={() => this.props.dispatch({type: 'BUTTON_ONE'})}>Button One</button>
-        <button onClick={() => this.props.dispatch({type: 'BUTTON_TWO'})}>Button Two</button>
+        <button onClick={() => this.props.dispatch({type: 'ADD'})}>Add</button>
+        <button onClick={() => this.props.dispatch({type: 'SUBTRACT'})}>Subtract</button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-const addReducer = (state, action) => {
-    console.log(`hey im reducer 1.`);
-    console.log(action);
-    return {};
+const counterReducer = (state = 0, action) => {
+    if(action.type === 'ADD'){
+        console.log(`hey im reducer add.`);
+        console.log(state + 1);
+        return state + 1;
+    }
+    if(action.type === 'SUBTRACT'){
+        console.log(`hey im reducer subtract.`);
+        console.log(state - 1);
+        return state - 1;
+    }
+    return state;
 }
 
 const subtractReducer = (state, action) => {
@@ -20,9 +29,10 @@ const subtractReducer = (state, action) => {
 const storeInstance = createStore(
     // this is called when the page LOADS or the DISPATCH is called
     combineReducers({
-        firstReducer,
-        secondReducer
-    })
+        counterReducer,
+        subtractReducer
+    }),
+    applyMiddleware(logger)
 )
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
